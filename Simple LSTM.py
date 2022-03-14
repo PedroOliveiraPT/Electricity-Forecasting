8851d91b261565244a1c61c89e28ab436d6c7082
Rafael Teixeira <rafaelgteixeira@ua.pt>
2022-03-14 18:31:38 +0000

    pca results

--- Simple LSTM.py
+++ Simple LSTM.py
@@ -6,8 +6,8 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px # to plot the time series plot
+#import matplotlib.pyplot as plt
+#import plotly.express as px # to plot the time series plot
 from sklearn import metrics # for the evaluation
 from sklearn.preprocessing import LabelEncoder,MinMaxScaler
 import tensorflow as tf 
